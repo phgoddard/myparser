60105c3d549cc064d6e4e7e026b5dd9dad95ee6e
tandemseven <tandemseven@Note245.fios-router.home>
2021-07-23 14:41:50 -0700

    updates on progress

--- app/myparser.py
+++ app/myparser.py
@@ -1,8 +1,49 @@
 import os
+import re
 
 class Dialog():
 
+    def __init__(self, filename: str):
+        self.cwd = str
+        self.WorkingDir = str
+        self.filename = filename
+        self.DialogLines = []
+        self.cleanListText = []
 
-def getFilename(filename: str):
-    return 'hello'+filename
+        self.cwd = self.getWorkingDir()
+        self.WorkingDir = self.changeWorkingDir()
+        self.DialogLines = self.readFile()
+        self.cleanListText = self.cleanNLandNumbers()
+
+    def getWorkingDir(self):
+        self.cwd = os.getcwd()
+        return self.cwd
+
+    def changeWorkingDir(self):
+        self.WorkingDir = os.chdir('/Users/tandemseven/Desktop/HLT Program/596A HLT Internship/Thematic-data/docusignresearchtranscriptthemetopicevaluation')
+        return os.getcwd()
+
+    def readFile(self):
+        self.DialogLines = []
+        for line in open(self.filename,'r'):
+            self.DialogLines.append(line)
+        return self.DialogLines
+
+    def cleanNLandNumbers(self):
+        self.cleanListText = []
+        rx = '^\d\d*\\n'
+        for line in self.DialogLines:
+            if line != "\n" and not re.search(rx,line):
+                self.cleanListText.append(line.strip())
+        return self.cleanListText
+
+if __name__ == "__main__":
+
+    d = Dialog('Docusign_p08.txt')
+    print(d.filename)
+    print(d.WorkingDir)
+    print(d.cwd)
+    print(d.DialogLines[:10])
+    print(d.cleanListText[:10])
+
 
--- tests/test_myparser.py
+++ tests/test_myparser.py
@@ -1,4 +1,19 @@
 from app.myparser import *
+import pytest
 
-def test_fileName():
-    assert getFilename('file') == 'hellofile'
+
+def test_getWorkingDir():
+    assert getWorkingDir() == '/Users/tandemseven/Desktop/HLT Program/508 Comp Tech for Linguists/Course Project/508_courseproject/tests'
+
+def test_changeWorkingDir():
+    assert changeWorkingDir() == '/Users/tandemseven/Desktop/HLT Program/596A HLT Internship/Thematic-data/docusignresearchtranscriptthemetopicevaluation'
+
+def test_getFileName():
+    assert getFileName() == 'Docusign_p08.txt'
+
+def test_readFile():
+    assert (readFile())[:3] == ['WEBVTT\n', '\n', '1\n']
+
+def test_cleanNLandNumbers():
+    listText = readFile()
+    assert cleanNLandNumbers()
