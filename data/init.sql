573d55ce9d412b8e51a0dacf18775dd1e0a78901
tandemseven <tandemseven@Note245.fios-router.home>
2021-07-31 10:23:02 -0700

    cleaned up init.sql

--- data/init.sql
+++ data/init.sql
@@ -2,24 +2,4 @@ CREATE DATABASE cxdata;
 ALTER DATABASE cxdata CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use cxdata;
 
-CREATE TABLE fileMetaData (
-    study_name VARCHAR(128),
-    study_id INT,
-    file_name VARCHAR(128),
-    file_id INT,
-    file_path VARCHAR(128),
-    output_filename VARCHAR(128),
-    all_text VARCHAR(16000000),
-    PRIMARY KEY (file_id)
-);
-
-CREATE TABLE dialog (
-    file_id INT,
-    line_number INT,
-    line_text text (65535),
-    clean_nlnum text (65535),
-    clean_timestamp text (65535),
-    speaker_name text (128),
-    PRIMARY KEY (file_id)
-);
 
